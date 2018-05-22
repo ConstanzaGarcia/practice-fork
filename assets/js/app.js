1f9e9172c5c1bc3d093ee3ab29629ff6e2255733
ConstanzaGarcia <o.conii.o@gmail.com>
2018-05-22 11:57:31 -0400

    Subiendo ejercicio con error

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+window.onload = fuction() {
+    var btn= document.getElementById("button-change");
+    var txt = document.getElementById("paragraph");
+    var btnDos= document.getElementById("magic");
+    var txtDos= document.getElementById("txt");
+
+    btn.addEventListener("click",function() {
+       txt.innerHTML= "<h2> Git & Github TE AMAMOS</h2>";
+    })
+
+    btnDos.addEventListener("click",fuction(){
+        //txtDos.InnerHTML= " ";
+        txtDos.classList.add("disappear");
+
+    } )
+
+}
